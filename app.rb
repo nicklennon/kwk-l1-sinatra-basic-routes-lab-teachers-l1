5da3ccaa31f9c3794d358ea448a7ff22078baec2
nicklennon <nicklennon.53@gmail.com>
2018-07-17 01:33:30 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do
+  "Hello, World!"
+  end
+  
+  get '/name' do
+    "My name is Nick"
+  end
+
+  get '/hometown' do
+    "My hometown is Cincinnati"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is This Time Tomorrow"
+  end
+
 end
